79ef7570d26bfc49f2844a384082e9943addf1da
cliffgor <clierozigy@gmail.com>
2021-01-29 00:49:23 +0300

    Added a repository that helps handle network errors incase of a disconnection

--- app/src/main/java/com/cliff/currencyconverter/main/MainRepository.kt
+++ app/src/main/java/com/cliff/currencyconverter/main/MainRepository.kt
@@ -1,5 +1,9 @@
 package com.cliff.currencyconverter.main
 
+import com.cliff.currencyconverter.data.models.CurrencyResponse
+import com.cliff.currencyconverter.util.Resource
+
 interface MainRepository {
-    
+
+    suspend fun getRates(base: String): Resource<CurrencyResponse>
 }
